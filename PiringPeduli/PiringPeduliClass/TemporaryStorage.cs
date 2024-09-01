c26fd3dea6ade316b469f36ea3f1cb69c46af71d
Nazrililham090805 <mmohnazrililham@gmail.com>
2024-09-01 16:59:09 +0700

    Add Class
    Add Class based on Class Diagram. Only added attributes, still have to add methods in the future

---
+++ PiringPeduli/PiringPeduliClass/TemporaryStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiringPeduliClass
+{
+    public class TemporaryStorage: Account
+    {
+        int storageId;
+        List<SortedWaste> storedWasteType;
+        string storageAddress;
+
+    }
+}
